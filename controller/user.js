588eb06eed599724b5146171fbeab0fecfcbcbdb
ankurraj <ankurrajdutta@gmail.com>
2022-09-24 22:40:47 +0530

    Task5

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,23 @@
 const user=require('../model/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 exports.addUser=(req,res,next)=>{
    const userName=req.body.userName;
    const userMail=req.body.userMail;
    const userPassword=req.body.userPassword;
-   user.create({name:userName,
-   email:userMail,
-   password:userPassword 
+   bcrypt.hash(userPassword, saltRounds).then(function(hash) {
+      return user.create({name:userName,
+         email:userMail,
+         password:hash 
+         })
    }).then(result=>{
-      res.status(200).json({success:true,message:'its working'})
+      return res.status(200).json({success:true,message:'User Created'})
    }).catch(err=>{
       return res.status(400).json({success:false,message:'something went wrong'})   
-   })
+   });
+
+  
 }
 
 exports.loginUser=(req,res,next)=>{
@@ -22,12 +28,14 @@ exports.loginUser=(req,res,next)=>{
          email:userMail
       }
    }).then(result=>{
-         if(result.password==userPassword){
-            res.status(200).json({success:true,message:'User Log in Successful'})
-         }else
-         {
-            res.status(401).json({success:false,message:'User Not Authorized'})
-         }
+         bcrypt.compare(userPassword, result.password).then(function(Cresult) {
+              if(Cresult==true){
+               res.status(200).json({success:true,message:'User Log in Successful'})
+              }else
+              {
+                 res.status(401).json({success:false,message:'User Not Authorized'})
+              }
+         })      
    }).catch(err=>{
       res.status(404).json({success:false,message:'User Not Found'})
    })
