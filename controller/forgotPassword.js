a605a240ef3d799a67a0e427af2e507577eaab66
ankurraj <ankurrajdutta@gmail.com>
2022-10-04 22:16:34 +0530

    Task11

--- controller/forgotPassword.js
+++ controller/forgotPassword.js
@@ -1,3 +1,34 @@
+let user=require('../model/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 exports.getPassword=(req,res,next)=>{
-    res.send('Its Working')
+   
+    let fiveDigit=Math.random()*100000;
+    fiveDigit=Math.ceil(fiveDigit)
+    let fiveString=fiveDigit.toString();
+    let userEmail=req.body.emailObj;
+    user.findOne({
+        where:{
+            email:userEmail
+        }
+    }).then(userData=>{
+        bcrypt.hash(fiveString, saltRounds).then(function(hash){
+        return userData.update({
+                password:hash
+            })
+        }).then(response=>{
+             return res.status(200).json({success:true,message:"User new password updated",fiveString})
+        }).catch(err=>{
+            console.log(err)
+            return res.status(400).json({success:false,message:"Something went wrong"})
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+        return res.status(400).json({success:false,message:"Something went wrong"})
+    })
+
+   
 }
--- public/script/forgotPassword.js
+++ public/script/forgotPassword.js
@@ -2,5 +2,10 @@ document.getElementsByClassName('btn')[0].addEventListener('click',(e)=>{
     e.preventDefault()
     const email=document.getElementById('exampleInputEmail1').value
     console.log(email)
-    axios.post('http://localhost:3000/password/forgotpassword',{emailObj:email}).then(res=>console.log(res)).catch(err=>console.log(err));
+    axios.post('http://localhost:3000/password/forgotpassword',{emailObj:email}).then(res=>{
+        console.log(res)
+        alert(`User New Password ${res.data.fiveString}`)
+        window.location.href='../login/login.html';
+    }).catch(err=>console.log(err));
+        
 })
